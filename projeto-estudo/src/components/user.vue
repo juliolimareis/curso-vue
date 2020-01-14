80889be833220cc6901a2d616e86ff92a85f2a7e
Júlio César Lima Reis <juliolimareis@gmail.com>
2020-01-13 23:43:02 -0300

    estudando componentes

---
+++ projeto-estudo/src/components/user.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>User</h1>
+        <div> {{ nome }} </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        nome:{
+            type:String,
+            // required:true
+            default:'Anonimo'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
